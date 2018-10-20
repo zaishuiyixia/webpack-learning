aca1e23cbaf7f43cbc628c22bee760fb3ae8e73f
zaishuiyixia <1720966051@qq.com>
2018-10-20 19:26:49 +0800

    update

---
+++ webpack(3)/3-3编译es6/dist/app.6ebf2874.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app.js":
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nTypeError: this.setDynamic is not a function\\n    at PluginPass.pre (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\babel-plugin-transform-runtime\\\\lib\\\\index.js:31:12)\\n    at transformFile (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:78:27)\\n    at runSync (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:45:3)\\n    at transformSync (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:43:38)\\n    at Object.transform (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:22:38)\\n    at transpile (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:55:20)\\n    at Object.module.exports (C:\\\\dream\\\\github\\\\webpack-learning\\\\webpack(3)\\\\3-3编译es6\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:179:20)\");\n\n//# sourceURL=webpack:///./app.js?");
+
+/***/ })
+
+/******/ });
